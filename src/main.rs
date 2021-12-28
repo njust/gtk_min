5c6904ed17ee412998dbd7f2be49cf8d74dd5add
Nico Just <mail@njust.de>
2021-12-28 22:43:23 +0100

    WIP: windows build

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use gtk4_helper::{
-    gtk,
     gtk::prelude::*,
     gtk::{Application, ApplicationWindow}
 };
+use sourceview5::prelude::*;
+
 
 fn main() {
     let app = Application::builder()
@@ -17,6 +18,16 @@ fn main() {
             .title("Hello, World!!")
             .build();
 
+        let buffer = sourceview5::Buffer::new(None);
+        let view = sourceview5::View::with_buffer(&buffer);
+        view.set_monospace(true);
+        view.set_background_pattern(sourceview5::BackgroundPatternType::Grid);
+        view.set_show_line_numbers(true);
+        view.set_highlight_current_line(true);
+        view.set_tab_width(4);
+        view.set_hexpand(true);
+
+        window.set_child(Some(&view));
         window.show();
     });
 
