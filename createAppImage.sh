11b9351c32f95ca936c7e7a21c88f3c7c2879048
Nico Just <mail@njust.de>
2021-12-28 01:28:10 +0100

    Initial checkin

--- createAppImage.sh
+++ createAppImage.sh
@@ -16,9 +16,9 @@ mkdir "$LIB_DIR"
 mkdir "$SHARE_DIR"
 
 LIBS=("libgtk-4.so.1.500.0" "libgtk-4.so.1" "libgtk-4.so" "libgio-2.0.so.0.7100.0" "libgio-2.0.so.0" "libgio-2.0.so" "libglib-2.0.so.0.7100.0" "libglib-2.0.so.0" "libglib-2.0.so"
-"libgtksourceview-5.so.0.0.0" "libgtksourceview-5.so.0" "libgtksourceview-5.so" "libgobject-2.0.so.0.7100.0" "libgobject-2.0.so.0" "libgobject-2.0.so" "libpango-1.0.so.0.5000.0"
-"libpango-1.0.so.0" "libpango-1.0.so" "libgraphene-1.0.so.0.1000.7" "libgraphene-1.0.so.0" "libgraphene-1.0.so" "libpangoft2-1.0.so.0.5000.0" "libpangoft2-1.0.so.0" "libpangoft2-1.0.so"
-"libpangocairo-1.0.so.0.5000.0" "libpangocairo-1.0.so.0" "libpangocairo-1.0.so" "libpangoxft-1.0.so.0.5000.0" "libpangoxft-1.0.so.0" "libpangoxft-1.0.so" "libgmodule-2.0.so.0.7100.0"
+"libgtksourceview-5.so.0.0.0" "libgtksourceview-5.so.0" "libgtksourceview-5.so" "libgobject-2.0.so.0.7100.0" "libgobject-2.0.so.0" "libgobject-2.0.so" "libpango-1*"
+"libgraphene-1.0.so.0.1000.7" "libgraphene-1.0.so.0" "libgraphene-1.0.so" "libpangoft2*"
+"libpangocairo*" "libpangoxft*" "libgmodule-2.0.so.0.7100.0"
 "libgmodule-2.0.so.0" "libgmodule-2.0.so" "libgthread-2.0.so.0.7100.0" "libgthread-2.0.so.0" "libgthread-2.0.so"
 )
 
