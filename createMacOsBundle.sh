74fcef3a6792bf50003ffacf4ca0a0b615cd3aa8
Nico Just <mail@njust.de>
2022-01-17 21:52:15 +0100

    WIP: macos build

--- createMacOsBundle.sh
+++ createMacOsBundle.sh
@@ -73,6 +73,7 @@ cp "$LIB_SRC/libXrender.1.dylib" "$LIB_DIR"
 cp "$LIB_SRC/libX11.6.dylib" "$LIB_DIR"
 cp "$LIB_SRC/libXext.6.dylib" "$LIB_DIR"
 cp "$LIB_SRC/libXau.6.dylib" "$LIB_DIR"
+cp "$LIB_SRC/libXdmcp.6.dylib" "$LIB_DIR"
 #cp "$LIB_SRC/libssl.1.1.dylib" "$LIB_DIR"
 
 mkdir "$LIB_DIR/gdk-pixbuf-2.0"
