dcc8bbdad7d87eee4cba6c0ff172ff42dc30ce7e
Nico Just <mail@njust.de>
2022-01-21 14:19:23 +0100

    WIP: macos build

--- src/main.rs
+++ src/main.rs
@@ -19,8 +19,7 @@ fn main() {
     app.connect_activate(|app| {
         let window = gtk::ApplicationWindow::new(app);
         window.set_title(Some("KTail"));
-        window.set_width_request(1600);
-        window.set_height_request(768);
+        window.set_default_size(1280, 800);
         let window = Rc::new(window);
 
         let (tx, rx) = glib::MainContext::channel(glib::PRIORITY_DEFAULT);
