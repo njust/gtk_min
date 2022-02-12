2d4414615677f7e08f18170fe887ab6376d8d3e3
Nico Just <mail@njust.de>
2022-02-12 20:23:20 +0100

    WIP: linux build

--- createAppImage.sh
+++ createAppImage.sh
@@ -49,7 +49,7 @@ do
   cp -r "/opt/gtk/lib/x86_64-linux-gnu/$LIB"* $LIB_DIR
 done
 
-cp -r "/opt/gtk/lib/x86_64-linux-gnu/libgtksourceview-5*" $LIB_DIR
+cp -r "/opt/gtkSourceView/lib/x86_64-linux-gnu/libgtksourceview-5*" $LIB_DIR
 
 ls -l $LIB_DIR
 
